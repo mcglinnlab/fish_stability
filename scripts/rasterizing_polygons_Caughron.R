19c7e453efa9a8339e36c30cff61b1b0c43055a5
Juliane Caughron <caughronjg@g.cofc.edu>
2019-04-08 15:23:55 -0400

    Updated rasterizing process

--- scripts/rasterizing_polygons_Caughron.R
+++ scripts/rasterizing_polygons_Caughron.R
@@ -9,7 +9,7 @@ library(maps)
 library(maptools)
 
 # read in the ocean
-oceans <- readOGR(dsn = "./Environment", layer = "ne_10m_ocean")
+oceans <- readOGR(dsn = "./shapefiles/ocean_raster", layer = "ne_10m_ocean")
 
 # create a global raster layer
 oceans <- spTransform(oceans, CRS("+proj=cea +units=km"))
@@ -17,32 +17,35 @@ oceans_raster <- raster(oceans)
 res(oceans_raster) <- 50
 
 # saving the world raster grid
-save(oceans_raster, file = './Data/raster/oceans_raster.Rdata')
+save(oceans_raster, file = './data/raster/oceans_raster.Rdata')
 load('./Data/raster/oceans_raster.Rdata')
 
 
-# making continents polygon ## where is continent data coming from? 
-continents <- shapefile('./data/continent/continent')
+# making continents polygon  
+continents <- shapefile('./shapefiles/continent/continent/continent.shp')
 continents <- spTransform(continents, CRS("+proj=cea +units=km"))
 
 
 
 #Raterizing SEAMAP-SA Data
-#SEAMAP_sub <- read.csv('./data/Environment/temp.csv')
-#Trawl_coord <-c('SEAMAP_sub$LATITUDESTART', 'SEAMAP_sub$LONGITUDESTART', 'SEAMAP_sub$COLLECTIONNUMBER')
-coordinates(Trawl_coord) <- ~LONGITUDE + LATITUDE
-proj4string(Trawl_coord) <- "+proj=longlat +datum=WGS84"
+SEAMAP_sub <- read.csv('./data/SEAMAP_sub.csv')
+Trawl_coord <- SEAMAP_sub[,c("LONGITUDESTART","LATITUDESTART", "COLLECTIONNUMBER")]
+Trawl_coord$LATITUDESTART <- as.numeric(as.character(Trawl_coord$LATITUDESTART))
+Trawl_coord$LONGITUDESTART <- as.numeric(as.character(Trawl_coord$LONGITUDESTART))
+Trawl_coord$COLLECTIONNUMBER <- as.numeric(as.character(Trawl_coord$COLLECTIONNUMBER))
+coordinates(Trawl_coord) <- ~ LONGITUDESTART + LATITUDESTART
+proj4string(Trawl_coord) <- "+proj=cea +units=km"
 Trawl_coord <- spTransform(Trawl_coord, CRS("+proj=cea +units=km"))
-Trawl_raster <- rasterize(Trawl_coord, oceans_raster, 'Meandepth')
-save(Trawl_raster, file = './data/raster/temp_raster.Rdata')
-load('./Data/raster/temp_raster.Rdata')
+Trawl_raster <- rasterize(Trawl_coord, oceans_raster)
+save(Trawl_raster, file = './data/raster/trawl_raster.Rdata')
+load('./data/raster/trawl_raster.Rdata')
 
 
 
 # Enivironmental variables
 
 # create a temperature raster
-temp <- read.csv('./Environment/temp.csv')
+temp <- read.csv('./data/environmentaldata/temp.csv')
 temp$Meandepth <- rowMeans(temp[,3:87], na.rm = TRUE)
 coordinates(temp) <- ~LONGITUDE + LATITUDE
 proj4string(temp) <- "+proj=longlat +datum=WGS84"
@@ -52,7 +55,7 @@ save(temp_raster, file = './data/raster/temp_raster.Rdata')
 load('./data/raster/temp_raster.Rdata')
 
 # create a salinity raster
-salinity <- read.csv('./Environment/salinity.csv')
+salinity <- read.csv('./data/environmentaldata/salinity.csv')
 salinity$Meandepth <- rowMeans(salinity[,3:86], na.rm = TRUE)
 coordinates(salinity) <- ~ Longitude + Latitude
 proj4string(salinity) <- "+proj=longlat +datum=WGS84"
@@ -60,6 +63,7 @@ salinity <- spTransform(salinity, CRS("+proj=cea +units=km"))
 salinity_raster <- rasterize(salinity, oceans_raster, 'Meandepth')
 save(salinity_raster, file = './data/raster/salinity_raster.Rdata')
 load('./data/raster/salinity_raster.Rdata')
+
 
 
 
