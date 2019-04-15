d435254a687d57c9759c488e9cb72143d62d3d94
Juliane Caughron <caughronjg@g.cofc.edu>
2019-04-14 22:39:34 -0400

    Temp map comes up but choppy, can't get trawl points to show up

--- scripts/Creating_raterized_maps_Caughron.R
+++ scripts/Creating_raterized_maps_Caughron.R
@@ -7,41 +7,22 @@ library(doParallel)
 library(foreach)
 library(maps)
 library(maptools)
+library(leaflet)
 
 #Creating Map
 
 
+## map experiment Trawl Data ## How to fix rasterizing to get data to show up? 
+
+m <- leaflet() %>% setView(lng = -79.2532, lat = 32.8484, zoom = 5)
+m %>% addTiles() %>%
+  addRasterImage(Trawl_raster, opacity = 0.8)
 
 
-#Enviroplot
-# enviro_plot function to aggregate and plot environmental variables
-# y = environmental raster from initial_cleanup
-# w = file path for pdf
-# output is raster list, name and save accordingly
+## map experiment Temp Data ## Why is temp so choppy? Can be fixed? 
 
-enviro_plot <- function(y, w) {
-  factor_val <- c(2, 4, 8, 16, 32)
-  enviro_list <- lapply(factor_val, function (x)
-    aggregate(y, fac = x, fun = mean))
-  enviro_list <- c(y, enviro_list)
-  return(enviro_list)
-  pdf(w)
-  for (i in 1:6) {
-    plot(enviro_list[[i]], main = paste('resolution =', res(enviro_list[[i]])))
-    plot(continents, add = T, col = "black")
-  }
-  dev.off()
-}
-
-
-#temp plot
-temp_list <- enviro_plot(temp_raster, './figures/temperature.pdf')
-save(temp_list, file = './data/raster/temp_list.Rdata')
-load('./data/raster/temp_list.Rdata')
-
-
-
-
-
+m <- leaflet() %>% setView(lng = -79.2532, lat = 32.8484, zoom = 5)
+m %>% addTiles() %>%
+  addRasterImage(temp_raster, opacity = 0.8)
 
 
