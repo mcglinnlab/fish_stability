253ec8f3dc555fc031aebe8636056154694cbbef
Juliane Caughron <caughronjg@g.cofc.edu>
2019-04-16 16:03:40 -0400

    added basic plots to beginning

--- scripts/Creating_raterized_maps_Caughron.R
+++ scripts/Creating_raterized_maps_Caughron.R
@@ -9,9 +9,20 @@ library(maps)
 library(maptools)
 library(leaflet)
 
+
+## basic plots ##
+
+map('state')
+points(SEAMAP_sub2[,c("LONGITUDESTART","LATITUDESTART")], col='red')
+
+map('world')
+points(SEAMAP_sub2[,c("LONGITUDESTART","LATITUDESTART")], col='red')
+axis(side=1)
+
+
+
 #Creating Map
 
-### same error comes up for both maps in rgdal: : rawTransform(projfrom, projto, ...): 54 projected points not finite ###
 
 ## map experiment Trawl Data ## How to fix rasterizing to get data to show up? 
 
@@ -22,8 +33,18 @@ m %>% addTiles() %>%
 
 ## map experiment Temp Data ## Why is temp so choppy? Can be fixed? 
 
-m <- leaflet() %>% setView(lng = -79.2532, lat = 32.8484, zoom = 5)
-m %>% addTiles() %>%
-  addRasterImage(temp_raster, opacity = 0.8)
+pal <- colorNumeric(palette = "YlGnBu", domain = temp_raster$layer)
 
+t <- leaflet() %>% setView(lng = -79.2532, lat = 32.8484, zoom = 5)
+t %>% addTiles() %>%
+  addRasterImage(temp_raster, opacity = 0.8) %>%
+  addLegend("bottomright", pal = pal, values= ~temp_raster$layer, title = "SST", opacity = 1)
+
+
+## map experiment Salinity ## 
+
+s <- leaflet() %>% setView(lng = -79.2532, lat = 32.8484, zoom = 5)
+s %>% addTiles() %>%
+  addRasterImage(salinity_raster, opacity = 0.8) %>%
+  addLegend("bottomright", pal = pal, values= ~salinity_raster$layer, title = "Salinity", opacity = 1)
 
