6973fa243c67e83cdcb4cb1a822264bcf61e4261
Juliane Caughron <caughronjg@g.cofc.edu>
2019-07-04 16:03:40 -0400

    Trying to sort out raster plot

--- scripts/rasterizing_polygons_Caughron.R
+++ scripts/rasterizing_polygons_Caughron.R
@@ -12,7 +12,7 @@ library(maptools)
 oceans <- readOGR(dsn = "./shapefiles/ocean_raster", layer = "ne_10m_ocean")
 
 # create a global raster layer
-oceans <- spTransform(oceans, CRS("+proj=cea +units=km +lat_0=32.4 +lon_0=-79.6"))
+oceans <- spTransform(oceans, CRS("+proj=longlat +units=km +lat_0=32.4 +lon_0=-79.6"))
 oceans_raster <- raster(oceans)
 res(oceans_raster) <- 50
 
@@ -23,7 +23,7 @@ load('./Data/raster/oceans_raster.Rdata')
 
 # making continents polygon  
 continents <- shapefile('./shapefiles/continent/continent/continent.shp')
-continents <- spTransform(continents, CRS("+proj=cea +units=km +lat_0=32.4 +lon_0=-79.6"))
+continents <- spTransform(continents, CRS("+proj=longlat +units=km +lat_0=32.4 +lon_0=-79.6"))
 
 
 
@@ -31,7 +31,7 @@ continents <- spTransform(continents, CRS("+proj=cea +units=km +lat_0=32.4 +lon_
 SEAMAP_sub <- read.csv('./data/SEAMAP_sub.csv')
 
 #subsetting columns
-Trawl_coord <- SEAMAP_sub2[,c("LONGITUDESTART","LATITUDESTART", "COLLECTIONNUMBER")]
+Trawl_coord <- SEAMAP_sub[,c("LONGITUDESTART","LATITUDESTART", "COLLECTIONNUMBER")]
 
 #taking out repeats of the collection number
 Trawl_coord <- Trawl_coord[!duplicated(Trawl_coord),]
@@ -41,23 +41,29 @@ Trawl_coord$LATITUDESTART <- as.numeric(as.character(Trawl_coord$LATITUDESTART))
 Trawl_coord$LONGITUDESTART <- as.numeric(as.character(Trawl_coord$LONGITUDESTART))
 Trawl_coord$COLLECTIONNUMBER <- as.numeric(as.character(Trawl_coord$COLLECTIONNUMBER))
 
-# adding identity column called trawl number
+#adding identity column called trawl number
 Trawl_coord$TRAWLNUMBER <- 1
 
 #setting lat and long columns and projection
 coordinates(Trawl_coord) <- ~ LONGITUDESTART + LATITUDESTART
-proj4string(Trawl_coord) <- "+proj=cea +units=km +lat_0=32.4 +lon_0=-79.6"
+#proj4string(Trawl_coord) <- "+proj=longlat +units=km +lat_0=32.4 +lon_0=-79.6"
 
 
-Trawl_coord <- spTransform(Trawl_coord, CRS("+proj=cea +units=km"))
+#rasterizing trawl sum data
+Trawl_raster <- rasterize(Trawl_coord, oceans_raster, Trawl_coord$TRAWLNUMBER, fun = sum)
+res(Trawl_raster)
+# When I plot here it gives me one large green square with 8000 trawls within so I tried adjusting resolution below#
+#plot(Trawl_raster)
+
+Trawl_raster.disaggregate <- disaggregate(Trawl_raster, fact=10)
+res(Trawl_raster.disaggregate)
+#With resolution increased by 10 fold still shows same big green square with 8000 trawls. Not sure why its not becoming more detailed. 
+#plot(Trawl_raster.disaggregate)
 
 
-## Need to figure out how to sum trawls within a raster region and show that in raster ## 
-## I think this should work to sum number of trawls in a raster region ##
-
-Trawl_raster <- rasterize(Trawl_coord, oceans_raster, sum(Trawl_coord$TRAWLNUMBER))
-save(Trawl_raster, file = './data/raster/trawl_raster.Rdata')
-load('./data/raster/trawl_raster.Rdata')
+#saving Trawl Raster File
+#save(Trawl_raster, file = './data/raster/trawl_raster.Rdata')
+#load('./data/raster/trawl_raster.Rdata')
 
 
 
