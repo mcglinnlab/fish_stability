90b219b48215f881489f13d86428dc995c2baaca
Juliane Caughron <caughronjg@g.cofc.edu>
2019-08-05 11:37:32 -0400

    Creating skeleton for code for creating plot of DSR relationship

---
+++ scripts/DSR_relationship.R
@@ -0,0 +1,12 @@
+
+#DSR Relationship
+
+#load data needed: data organized into raster regions, varience in biomass through time for raster region and
+#cumulative diversity for raster region
+
+#sum biomass for all collections within each respective raster region for each year
+#calculate variance through time for biomass within each raster region
+#calculate average species number for each respective raster region
+
+#create new data frame with columns: raster region ID, var in biomass, average species richness, possibly geographic description (ex "Raleigh Bay, SC, GA")
+#plot var in biomass as a function of species richness 
