029657eb34d411b13f7e1e99a078910898b41c3e
Juliane Caughron <caughronjg@g.cofc.edu>
2019-10-15 19:18:24 -0400

    plots for poster

--- scripts/Investigations.R
+++ scripts/Investigations.R
@@ -17,8 +17,9 @@ xyplot(s_rarefac$biomass ~ s_rarefac$S|s_rarefac$year , relation = 'free')
 xyplot(s_rarefac$biomass ~ s_rarefac$S, groups = s_rarefac$year)
        
 
-
-
-
+lm1995 <- lm(s_rarefac$biomass[s_rarefac$year == '1995'] ~ s_rarefac$S[s_rarefac$year == '1995'])
+plot(s_rarefac$biomass[s_rarefac$year == '1995'] ~ s_rarefac$S[s_rarefac$year == '1995'])
+abline(lm1995$coefficients)
+summary(lm1995)
 
 
