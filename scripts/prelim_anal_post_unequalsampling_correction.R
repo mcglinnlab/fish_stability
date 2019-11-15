3e10c6c913d579d4675eea78ee96549c554f93bf
Juliane Caughron <caughronjg@g.cofc.edu>
2019-11-15 11:24:33 -0500

    working on data analysis and graphing

--- scripts/prelim_anal_post_unequalsampling_correction.R
+++ scripts/prelim_anal_post_unequalsampling_correction.R
@@ -1,32 +1,62 @@
 #Biomass as a function of species richness post unequal sampling correction
 
+
+#in permutation is it still ignoring grids with only one trawl? 
+#those rasters with only one site do not have a variation. Complete cases removes these raster regions from the results data frame
+
 resultsfullpoint2 <- read.csv( "~./fish_stability/data/resultsfullpoint2.csv", header = T)
+completeresultsfullpoint2 <- resultsfullpoint2[complete.cases(resultsfullpoint2),]
 
 #plot of average biomass per raster region per year as a function of average species richness per year
 
 for (i in 1989:2015) {
-  plot(resultsfullpoint2$averagebio[resultsfullpoint2$year == i] ~ resultsfullpoint2$averageS[resultsfullpoint2$year == i], xlab = "Average Species Richness", ylab = "Average Biomass", main = i)
+  plot(completeresultsfullpoint2$averagebio[resultsfullpoint2$year == i] ~ completeresultsfullpoint2$averageS[resultsfullpoint2$year == i], xlab = "Average Species Richness", ylab = "Average Biomass", main = i)
 }
 
-# plot of var in biomass per raster as a function of year per raster 
-
-for (i in unique(resultsfullpoint2$ID)) {
-  plot(resultsfullpoint2$varbio[resultsfullpoint2$ID == i] ~ resultsfullpoint2$year[resultsfullpoint2$ID == i], ylim = c(0,2000000), ylab = "Variance in Biomass", xlab = "Year", main = i)
+# plot of var in biomass per raster as a function of year per raster
+# var bio shows variance in trawls within a raster region when resampled 
+for (j in unique(resultsfullpoint2$ID)) {
+  plot(completeresultsfullpoint2$varbio[completeresultsfullpoint2$ID == j] ~ completeresultsfullpoint2$year[completeresultsfullpoint2$ID == j], ylim = c(0,2000000), ylab = "Variance in Biomass", xlab = "Year", main = j)
 }
 
 #plot of var in biomass per raster as a function of average species richness per raster 
 
-for (j in unique(resultsfullpoint2$ID)) {
+for (k in unique(resultsfullpoint2$ID)) {
   
-   plot(resultsfullpoint2$varbio[resultsfullpoint2$ID == j] ~ resultsfullpoint2$averageS[resultsfullpoint2$ID == j], ylim = c(0,1000), ylab = "Var in Biomass", xlab = "Average Species Richness", main = i)
+   plot(completeresultsfullpoint2$varbio[completeresultsfullpoint2$ID == k] ~ completeresultsfullpoint2$averageS[completeresultsfullpoint2$ID == k], ylim = c(0,1000), xlim = c(0,40), ylab = "Var in Biomass", xlab = "Average Species Richness", main = k)
+}
+
+# this each point on this plot is one raster region
+
+plot(completeresultsfullpoint2$averagebio ~ completeresultsfullpoint2$year)
+
+# biomass as a function of species richness for each raster region
+for (k in unique(resultsfullpoint2$ID)) {
+  
+  plot(completeresultsfullpoint2$averagebio[completeresultsfullpoint2$ID == k] ~ completeresultsfullpoint2$averageS[completeresultsfullpoint2$ID == k], ylim = c(0,1000), xlim = c(0,40), ylab = "Biomass", xlab = "Average Species Richness", main = k)
 }
 
 #collapsing graph above to create one point for each raster region of var and average species richness through time
-for (j in unique(resultsfullpoint2$ID)) {
-  averagevarbiovector[j] <- average(resultsfullpoint2$varbio[resultsfullpoint2$ID == j])
-  averagespeciesrichvector[j] <- average(resultsfullpoint2$averageS[resultsfullpoint2 == j])
-  ID <- j
+
+averagevarbiovector <- NULL
+averagespeciesrichvector <- NULL
+avvarspeciesresults <- NULL
+ID <- NULL
+vec <- unique(resultsfullpoint2$ID)
+
+for (m in vec) {
+  averagevarbiovector <- c(averagevarbiovector, var(completeresultsfullpoint2$averagebio[completeresultsfullpoint2$ID == m]))
+  averagespeciesrichvector <- c(averagespeciesrichvector, mean(completeresultsfullpoint2$averageS[completeresultsfullpoint2$ID == m]))
+  ID <- c(ID, m)
   avvarspeciesresults <- data.frame(ID, averagevarbiovector, averagespeciesrichvector)
 }
+
+#each point is a raster region averaged through time
+
+varspecieslm <- lm(avvarspeciesresults$averagevarbiovector ~ avvarspeciesresults$averagespeciesrichvector)
+plot(avvarspeciesresults$averagevarbiovector ~ avvarspeciesresults$averagespeciesrichvector)
+abline(varspecieslm$coefficients)
+
+summary(varspecieslm)
 
 
