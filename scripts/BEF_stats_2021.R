2c91b6d1088da8cd6d29563fa70759decdd4e232
Juliane Caughron <caughronjg@g.cofc.edu>
2021-04-10 15:37:13 -0400

    starting to make figures, maps

--- scripts/BEF_stats_2021.R
+++ scripts/BEF_stats_2021.R
@@ -1,12 +1,28 @@
 library(dplyr)
 library(tidyr)
+library(raster)
+library(tmap)
+library(tmaptools)
 
 #### BEF SCALE ONE ANALYSIS ####
 
-#DATA SET
+#DATA SETS
   #SUMMARY_BEF - 324,000  rows, 1,000 iterations, 36 rasters, 9 time bins
 summary_BEF <- read.csv("~/fish_stability/data/BEF/summary_BEF.csv", header = T)
 summary_BEF <- summary_BEF[, -1]
+
+  #ID.df - Information about each raster IDs
+ID.df <- read.csv("~/fish_stability/data/ID.df.csv", header = T)
+ID.df <- ID.df[, -1]
+
+  #ID list - list of good raster IDs
+IDlist <- c(1246, 1294, 1340, 1486, 1532, 1534, 1536, 1582, 1586, 1630, 1680, 
+            1730, 1777, 1778, 1826, 1875, 1923, 1924, 1972, 1973, 2021, 2022, 
+            2120, 2169, 2170, 2219, 2269, 2318, 2319, 2419, 2519, 2569, 2620, 
+            2670, 2721, 2771)
+  #rasterID_coords - coordinate of center of raster region
+rasterID_coord <- read.csv("~/fish_stability/data/rasterID_coord.csv", header = T)
+rasterID_coord <- rasterID_coord[, -1]
 
 
 #### Averaging across bootstrap iterations ####
@@ -132,8 +148,125 @@ with(BEF_yr, plot(log(b_yrav) ~ salS_yr))
 with(BEF_yr, plot(log(B_yrstab) ~ salS_yr))
 with(BEF_yr, plot(log(B_yrstab) ~ tempS_yr))
 
+with(BEF_yr, plot(log(b_shrimp_yrav) ~ tempS_yr))
+with(BEF_yr, plot(log(b_flounder_yrav) ~ tempS_yr))
+with(BEF_yr, plot(log(b_flounder_yrav) ~ log(b_yrav)))
+with(BEF_yr, plot(log(b_flounder_yrav) ~ log(S_yrav)))
+with(BEF_yr, plot(log(b_shrimp_yrav) ~ log(S_yrav)))
+
 
 
 mod1 <- with(BEF_yr, lm(log(b_yrav) ~ log(S_yrav)))
 with(BEF_yr, plot(log(b_yrav) ~ log(S_yrav)))
 abline(mod1$coefficients)
+summary(mod1)
+
+
+
+
+####FIGURE 1 - RASTER MAPS ####
+
+#a) number of trawls per raster - uses ID.df
+ID.df$identity <- 1
+
+trawl_num <- ID.df %>%
+  group_by(rasterID) %>%
+  summarize(trawlnum = sum(identity))
+
+trawl_num <- trawl_num[trawl_num$rasterID %in% IDlist, ]
+trawl_num <- cbind(trawl_num, rasterID_coord$x, rasterID_coord$y)
+names(trawl_num) <- c("ID", "trawlnum", "long", "lat")
+
+#extent of maps
+new.extent <- c(-84, -75, 27, 38)
+
+#36 good raster regions
+Trawl_raster <- rasterize(trawl_num[,3:4], oc_raster, trawl_num$trawlnum)
+res(Trawl_raster)
+plot(Trawl_raster)
+
+Trawl_raster <- crop(x = Trawl_raster, y = new.extent)
+
+tm_shape(Trawl_raster) +
+  tm_raster(title = "Number of Trawls") +
+  tm_shape(continents) +
+  tm_borders(lwd = 0.5) +
+  tm_scale_bar(position = c("left", "bottom")) +
+  tm_compass()
+
+#b) average rarefied richness to 5 trawls
+Species_Raster <- rasterize(trawl_num[, 3:4], oc_raster, BEF_yr$S_yrav)
+res(Species_Raster)
+plot(Species_Raster)
+
+Species_Raster <- crop( x = Species_Raster, y = new.extent)
+
+tm_shape(Species_Raster) +
+  tm_raster(title = "Number of Species (S)", palette = "Blues") +
+  tm_shape(continents) +
+  tm_borders(lwd = 0.5) +
+  tm_scale_bar(position = c("left", "bottom")) +
+  tm_compass()
+
+#c) average total biomass 5 trawls
+Biomass_Raster <- rasterize(trawl_num[, 3:4], oc_raster, BEF_yr$b_yrav)
+res(Biomass_Raster)
+plot(Biomass_Raster)
+
+Biomass_Raster <- crop(x = Biomass_Raster, y = new.extent)
+
+tm_shape(Biomass_Raster) +
+  tm_raster(title = " Fish Biomass (kg)", palette = "Greens") +
+  tm_shape(continents) +
+  tm_borders(lwd = 0.5) +
+  tm_scale_bar(position = c("left", "bottom")) +
+  tm_compass()
+
+#d) average stability biomass at 5 trawls
+Stability_Raster <- rasterize(trawl_num[, 3:4], oc_raster, BEF_yr$B_yrstab)
+res(Stability_Raster)
+plot(Stability_Raster)
+
+Stability_Raster <- crop(x = Stability_Raster, y = new.extent)
+
+tm_shape(Stability_Raster) +
+  tm_raster(title = "Stability of Fish Biomass", palette = "PuRd") +
+  tm_shape(continents) +
+  tm_borders(lwd = 0.5) +
+  tm_scale_bar(position = c("left", "bottom")) +
+  tm_compass()
+
+#e) average surface temperature
+SurfaceTemp_Raster <- rasterize(trawl_num[, 3:4], oc_raster,
+                                BEF_yr$tempS_yr)
+res(SurfaceTemp_Raster)
+plot(SurfaceTemp_Raster)
+
+SurfaceTemp_Raster <- crop(x = SurfaceTemp_Raster, y = new.extent)
+
+tm_shape(SurfaceTemp_Raster) +
+  tm_raster(title = "Surface Water Temperature (C)", palette = "-RdYlBu") +
+  tm_shape(continents) +
+  tm_borders(lwd = 0.5) +
+  tm_scale_bar(position = c("left", "bottom")) +
+  tm_compass() +
+  tm_layout(legend.text.size = 0.75, 
+            legend.title.size = 2)
+
+#f) average surface salinity
+SurfaceSal_Raster <- rasterize(trawl_num[, 3:4], oc_raster,
+                               BEF_yr$salS_yr)
+res(SurfaceSal_Raster)
+plot(SurfaceSal_Raster)
+
+SurfaceSal_Raster <- crop(x = SurfaceSal_Raster, y = new.extent)
+
+tm_shape(SurfaceSal_Raster) +
+  tm_raster(title = "", palette = "-RdYlGn") +
+  tm_shape(continents) +
+  tm_borders(lwd = 0.5) +
+  tm_scale_bar(position = c("left", "bottom")) +
+  tm_compass() +
+  tm_layout(legend.text.size = 0.75,
+            legend.title.size = 2)
+
